122cb95a88bba35daab7ceecc09349345b1d0491
rkerner <rkerner.mobil@gmail.com>
2023-11-07 20:13:17 +0100

    DBZ-6762 Add "validate filters" endpoint/s for connector-specific Connect REST Extensions
    closes to https://issues.redhat.com/browse/DBZ-6762

--- debezium-connector-oracle/src/test/java/io/debezium/connector/oracle/rest/DebeziumOracleConnectorResourceIT.java
+++ debezium-connector-oracle/src/test/java/io/debezium/connector/oracle/rest/DebeziumOracleConnectorResourceIT.java
@@ -20,6 +20,7 @@ import org.junit.Assume;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.testcontainers.utility.MountableFile;
 
 import io.debezium.connector.oracle.Module;
 import io.debezium.connector.oracle.OracleConnector;
@@ -35,6 +36,12 @@ public class DebeziumOracleConnectorResourceIT {
     public static void checkCondition() {
         Assume.assumeThat("Skipping DebeziumOracleConnectorResourceIT tests when assembly profile is not active!", System.getProperty("isAssemblyProfileActive", "false"),
                 is("true"));
+        try {
+            RestExtensionTestInfrastructure.getOracleContainer().withCopyFileToContainer(MountableFile.forClasspathResource("rest/data.sql"),
+                    "/opt/oracle/scripts/startup/data.sql");
+        }
+        catch (IllegalStateException ignored) {
+        }
     }
 
     @Before
@@ -97,6 +104,70 @@ public class DebeziumOracleConnectorResourceIT {
                                 Map.of("property", OracleConnectorConfig.TOPIC_PREFIX.name(), "message", "The 'topic.prefix' value is invalid: A value is required"),
                                 Map.of("property", OracleConnectorConfig.URL.name(), "message", "The 'database.url' value is invalid: A value is required"),
                                 Map.of("property", OracleConnectorConfig.HOSTNAME.name(), "message", "The 'database.hostname' value is invalid: A value is required")));
+    }
+
+    @Test
+    public void testFiltersWithEmptyFilters() {
+        ConnectorConfiguration config = getOracleConnectorConfiguration(1);
+
+        given()
+                .port(RestExtensionTestInfrastructure.getDebeziumContainer().getFirstMappedPort())
+                .when().contentType(ContentType.JSON).accept(ContentType.JSON).body(config.toJson())
+                .put(DebeziumOracleConnectorResource.BASE_PATH + DebeziumOracleConnectorResource.VALIDATE_FILTERS_ENDPOINT)
+                .then().log().all()
+                .statusCode(200)
+                .assertThat().body("status", equalTo("VALID"))
+                .body("validationResults.size()", is(0))
+                .body("matchingCollections.size()", is(6))
+                .body("matchingCollections",
+                        hasItems(
+                                Map.of("namespace", "C##DBZUSER", "name", "PEOPLE", "identifier", "C##DBZUSER.PEOPLE"),
+                                Map.of("namespace", "C##DBZUSER", "name", "DEBEZIUM_TABLE3", "identifier", "C##DBZUSER.DEBEZIUM_TABLE3"),
+                                Map.of("namespace", "C##DBZUSER", "name", "DEBEZIUM_TABLE1", "identifier", "C##DBZUSER.DEBEZIUM_TABLE1"),
+                                Map.of("namespace", "C##DBZUSER", "name", "DEBEZIUM_TABLE2", "identifier", "C##DBZUSER.DEBEZIUM_TABLE2"),
+                                Map.of("namespace", "C##DBZUSER", "name", "DEBEZIUM_TEST", "identifier", "C##DBZUSER.DEBEZIUM_TEST"),
+                                Map.of("namespace", "C##DBZUSER", "name", "TEST", "identifier", "C##DBZUSER.TEST")));
+    }
+
+    @Test
+    public void testFiltersWithValidTableIncludeList() {
+        ConnectorConfiguration config = getOracleConnectorConfiguration(1)
+                .with("table.include.list", "C##DBZUSER\\.DEBEZIUM_TABLE.*");
+
+        given()
+                .port(RestExtensionTestInfrastructure.getDebeziumContainer().getFirstMappedPort())
+                .when().contentType(ContentType.JSON).accept(ContentType.JSON).body(config.toJson())
+                .put(DebeziumOracleConnectorResource.BASE_PATH + DebeziumOracleConnectorResource.VALIDATE_FILTERS_ENDPOINT)
+                .then().log().all()
+                .statusCode(200)
+                .assertThat().body("status", equalTo("VALID"))
+                .body("validationResults.size()", is(0))
+                .body("matchingCollections.size()", is(3))
+                .body("matchingCollections",
+                        hasItems(
+                                Map.of("namespace", "C##DBZUSER", "name", "DEBEZIUM_TABLE3", "identifier", "C##DBZUSER.DEBEZIUM_TABLE3"),
+                                Map.of("namespace", "C##DBZUSER", "name", "DEBEZIUM_TABLE1", "identifier", "C##DBZUSER.DEBEZIUM_TABLE1"),
+                                Map.of("namespace", "C##DBZUSER", "name", "DEBEZIUM_TABLE2", "identifier", "C##DBZUSER.DEBEZIUM_TABLE2")));
+    }
+
+    @Test
+    public void testFiltersWithInvalidTableIncludeList() {
+        ConnectorConfiguration config = getOracleConnectorConfiguration(1)
+                .with("table.include.list", "+");
+
+        given()
+                .port(RestExtensionTestInfrastructure.getDebeziumContainer().getFirstMappedPort())
+                .when().contentType(ContentType.JSON).accept(ContentType.JSON).body(config.toJson())
+                .put(DebeziumOracleConnectorResource.BASE_PATH + DebeziumOracleConnectorResource.VALIDATE_FILTERS_ENDPOINT)
+                .then().log().all()
+                .statusCode(200)
+                .assertThat().body("status", equalTo("INVALID"))
+                .body("matchingCollections.size()", is(0))
+                .body("validationResults.size()", is(1))
+                .rootPath("validationResults[0]")
+                .body("property", equalTo("table.include.list"))
+                .body("message", equalTo(
+                        "The 'table.include.list' value is invalid: A comma-separated list of valid regular expressions is expected, but Dangling meta character '+' near index 0\n+\n^"));
     }
 
     public static ConnectorConfiguration getOracleConnectorConfiguration(int id, String... options) {
